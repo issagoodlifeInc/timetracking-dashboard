efe29f5c9a8ce9799b17684c91513d561883131f
Leskim <leskimuti@gmail.com>
2022-09-22 01:58:10 +0300

    Added components and imported react + exports where needed

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
+    Coded by <a href="https://leskimfamily.herokuapp.com/" target="_blank" rel='noreferrer'>Lesley Kimutai</a>.
+  </footer>
+  )
+}
+
+export default Footer
