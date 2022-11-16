5f9f9e80692a76ae3c1ac84bcfddf4a83e0656b2
Lesley <leskimuti@gmail.com>
2022-11-16 11:18:17 +0300

    eslint issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import styles from './styles.css';
-import App from './App';
-
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
-
-root.render(<App/>)
-
+root.render(<App />);
