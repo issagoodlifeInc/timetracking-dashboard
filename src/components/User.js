fc6018cbcba0175bc4a85f48267d9ddfc7b9854d
Lesley <leskimuti@gmail.com>
2023-02-28 22:48:02 +0300

    Styling the main using grid

--- src/components/User.js
+++ src/components/User.js
@@ -4,15 +4,18 @@ import jeremy from "../images/image-jeremy.png";
 export default function User() {
   return (
     <section className="user">
-      <div className="user-details">
+      <div className="user_details">
         <img src={jeremy} alt="Jeremy" />
-        <p className="user--text">Report for</p>
-        <h1 className="user--title">Jeremy Robson</h1>
+        <div className="user_dets">
+          <p className="user--text">Report for</p>
+          <h1 className="user--title">Jeremy Robson</h1>
+        </div>
       </div>
-
-      <button className="btn_timeframe">Daily</button>
-      <button className="btn_timeframe">Weekly</button>
-      <button className="btn_timeframe">Monthly</button>
+      <div className="btns">
+        <button className="btn_timeframe">Daily</button>
+        <button className="btn_timeframe">Weekly</button>
+        <button className="btn_timeframe">Monthly</button>
+      </div>
     </section>
   );
 }
